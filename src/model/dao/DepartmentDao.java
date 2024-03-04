0cf8a334714cae97dec43649ed174149a40a11a7
Nicolas <moreiranicolas2008@gmail.com>
2024-03-04 18:45:17 -0300

    Add class Dao Deparment and Seller

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import entities.Department;
+
+public interface DepartmentDao {
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findId(Integer id);
+	List<Department> findAll();
+}
